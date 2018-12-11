4742993e9bdedc7bdcf4c86ac76f056eb73e8252
shan-qi <1241016810@qq.com>
2018-12-11 16:15:48 +0800

    添加Podfile和PCH文件

---
+++ Yingke-TV/Yingke-TV/Classes/Other/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  Yingke-TV
+//
+//  Created by 小琦 on 2018/12/11.
+//  Copyright © 2018 单琦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Yingke-TV/Yingke-TV/Classes/Other/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  Yingke-TV
+//
+//  Created by 小琦 on 2018/12/11.
+//  Copyright © 2018 单琦. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
