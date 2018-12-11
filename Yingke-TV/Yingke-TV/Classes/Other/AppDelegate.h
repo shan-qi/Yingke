21155e46aa6fac45dcec6d55e7bab5c726f7e7a8
shan-qi <1241016810@qq.com>
2018-12-11 15:56:05 +0800

    分配跟新

---
+++ Yingke-TV/Yingke-TV/Classes/Other/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Yingke-TV
+//
+//  Created by 小琦 on 2018/12/11.
+//  Copyright © 2018 单琦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
